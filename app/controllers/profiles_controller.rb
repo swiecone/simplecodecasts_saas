18f5d74032c1131367aa5788372021039245524e
swiecone <alejandro.swiec@gmail.com>
2016-01-31 10:27:22 +0100

    Enabled the update of the Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -23,9 +23,16 @@ class ProfilesController < ApplicationController
 	end 
 
 	def update
-
+		@user = User.find( params[:user_id])
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Profile Updated!"
+			redirect_to user_path( params[:user_id])
+		else
+			render action: :edit
+		end
 	end 
-	
+
 
 	private 
 		def profile_params
