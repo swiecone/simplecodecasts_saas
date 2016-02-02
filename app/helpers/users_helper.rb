3c7124b98bf368f7b3d495560884c11f6fe0a115
swiecone <alejandro.swiec@gmail.com>
2016-02-02 12:56:26 +0100

    Addded some styling to the User Profiles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,11 +2,11 @@ module UsersHelper
 
 	def job_title_icon
 		if @user.profile.job_title == "Developer"
-			"<i class='fa fa-code text-center'></i>".html_safe
+			"<i class='fa fa-code'></i>".html_safe
 		elsif @user.profile.job_title == "Entrepreneur"
-			"<i class='fa fa-lightbulb-o text-center'></i>".html_safe
+			"<i class='fa fa-lightbulb-o'></i>".html_safe
 		else @user.profile.job_title == "Investor"
-			"<i class='fa fa-dollar text-center'></i>".html_safe
+			"<i class='fa fa-dollar'></i>".html_safe
 		end
 	end 
 end
